15d6bfd7d4c39b9f720bfe8955a8a0fef07892fe
John Andrews <johnandrews@Johns-MacBook.local>
2013-10-03 14:10:14 -0500

    Initial Commit

---
+++ WikipediaChuckNorris/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  WikipediaChuckNorris
+//
+//  Created by John Andrews on 10/3/13.
+//  Copyright (c) 2013 John Andrews. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
